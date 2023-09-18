c6ed8139b13d46ef7877318d351f751f9647cb03
kikedamo <davbaseka@hotmail.com>
2023-09-17 23:45:08 -0400

    Completes Lab 2.

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let favNumber = 3;
 let squared = favNumber * favNumber;
 
 console.log("My First Name is " + firstName)
-console.log('The Squared Value of',favNumber,'is', squared);
+console.log('The Squared Value of ' + favNumber,'is ' + squared);
